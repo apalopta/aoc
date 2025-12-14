bd562a21d1c3eb26294a085a975f89c73018a9ed
Anja Papatola <mamadev@apalopta.de>
2025-12-14 14:51:58 +0100

    2025-04 part 2

--- src/aoc2025/day04/Day04.kt
+++ src/aoc2025/day04/Day04.kt
@@ -13,14 +13,14 @@ fun main() {
     val testInput = readInput(2025, 4, "test")
 
     check(part1(testInput) == 13)
-//    check(part2(testInput) == 43)
+    check(part2(testInput) == 43)
 
     val input = readInput(2025, 4, "input")
 
     val res1 = part1(input)
     check(res1 == 1320)
     res1.printlnPrefixed("part 1")
-//    part2(input).printlnPrefixed("part 2")
+    part2(input).printlnPrefixed("part 2")
 }
 
 fun part1(input: List<String>): Int {
@@ -36,19 +36,33 @@ fun part1(input: List<String>): Int {
         .also { println(it) }
 }
 
+fun part2(input: List<String>): Int {
+    val arr = Array(input.size) { x ->
+        Array(input[x].length) { y -> Point(x, y, input[x][y]) }
+    }
+    var count = 0
+
+    do {
+        val list = replaceByXForAccessible(arr)
+        count += arr.toList().flatMap { it.toList() }.count { it.c == 'x' }
+        list.forEach { p -> arr[p.x][p.y] = p.copy(c = '.') }
+    } while (list.isNotEmpty())
+
+//    println(arr.draw())
+    return count
+        .also { println(it) }
+}
+
 private fun replaceByXForAccessible(
     arr: Array<Array<Point>>
-) {
-    val points: List<Point> = arr.toList().flatMap { it.toList() }
-    points.filter { point ->
-        point.countRollsAround(arr) < 4
-    }.filter { p -> p.c == '@' }
+): List<Point> {
+    return arr.toList().flatMap { it.toList() }
+        .filter { point ->
+            point.countRollsAround(arr) < 4
+        }.filter { p -> p.c == '@' }
         .also { it.forEach { p -> arr[p.x][p.y] = p.copy(c = 'x') } }
 }
 
-fun part2(input: List<String>): Int {
-    return 0
-}
 
 fun Array<Array<Point>>.draw() =
     this.joinToString(separator = "\n") { row ->
@@ -63,18 +77,18 @@ fun Point.countRollsAround(input: Array<Array<Point>>): Int {
     val minJ = (y - 1).coerceAtLeast(0)
     val maxJ = (y + 1).coerceAtMost(input[x].lastIndex)
 
-    print("=> [${x}:${y}] (i:$minI->$maxI, j:$minJ->$maxJ) : ")
+//    print("=> [${x}:${y}] (i:$minI->$maxI, j:$minJ->$maxJ) : ")
     for (i in minI..maxI) {
         for (j in minJ..maxJ) {
             val point = input[i][j]
             if ((!(point.x == this.x && point.y == this.y))
                 && (point.c == '@')
             ) {
-                print(" @ [$i:$j]")
+//                print(" @ [$i:$j]")
                 rolls++
             }
         }
     }
-    println(" -> $rolls")
+//    println(" -> $rolls")
     return rolls
 }
