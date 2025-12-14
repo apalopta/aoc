a9a8f540573e11db057a93e3eb9fecb4231881d7
Anja Papatola <mamadev@apalopta.de>
2025-12-14 22:06:26 +0100

    2025-05 part 1

--- src/aoc2025/day06/Day06.kt
+++ src/aoc2025/day06/Day06.kt
@@ -6,13 +6,13 @@ import utils.printlnPrefixed
 fun main() {
     val testInput = readInput(2025, 6, "test")
 
-    check(part1(testInput) == 0L)
+    check(part1(testInput) == 4277556L)
     check(part2(testInput) == 0L)
 
     val input = readInput(2025, 6, "input")
 
     part1(input)
-//        .also { check(it == ) }
+        .also { check(it == 5733696195703L) }
         .also { it.printlnPrefixed("part 1") }
     part2(input)
 //        .also { check(it == ) }
@@ -20,8 +20,27 @@ fun main() {
 }
 
 fun part1(input: List<String>): Long {
-    return 0L
+    val lines = input.map { line -> line.split(" ").filterNot(String::isEmpty) }
+    val elems = lines.transpose()
+    val res = elems.map {
+        println(it)
+        val operation = it.last()
+        val numbers = it.dropLast(1).map { n -> n.toLong() }
+        val res = when (operation) {
+            "*" -> numbers.fold(1L) { acc, number -> acc * number }
+            "+" -> numbers.fold(0L) { acc, number -> acc + number }
+            else -> TODO("not implemented")
+        }
+        res
+    }
+
+    return res.sum()
 }
+
+fun List<List<String>>.transpose() =
+    first().indices.map { col ->
+        map { it[col] }
+    }
 
 fun part2(input: List<String>): Long {
     return 0L
